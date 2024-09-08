8e810e46fe4465c0919eea928138924603eccf9b
christofertyler <51095969+christofertyler@users.noreply.github.com>
2024-09-08 18:07:22 -0400

    Update buttontest.py

--- Nightlight Part 1/buttontest.py
+++ Nightlight Part 1/buttontest.py
@@ -1,13 +1,13 @@
 import time
 from machine import Pin
 
-# Set up the built-in button on GPIO 20 with pull-down resistor
+# set up the built-in button (pin 20) 
 button = Pin(20, Pin.IN, Pin.PULL_UP)
 
-# Simple loop to check the button state
+# loop to check button state
 while True:
     if button.value() == 0:
-        print("Button pressed!")  # This will print whenever the button is pressed
+        print("Button pressed!")  # prints when button registers as pressed
     else:
-        print("Button not pressed")  # This will print when the button is not pressed
-    time.sleep(0.2)  # Add a small delay to avoid flooding the output
+        print("Button not pressed")  # prints when button registers as not pressed
+    time.sleep(0.2)  # small delay to avoid flooding response
